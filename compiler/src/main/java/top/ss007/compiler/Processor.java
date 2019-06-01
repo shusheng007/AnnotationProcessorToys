b6914c78fb2f5022a437a84142afe9de7b2e97e4
shusheng007 <568894336@qq.com>
2019-06-01 22:07:15 +0800

    feature：简化代码生成逻辑

--- compiler/src/main/java/top/ss007/compiler/Processor.java
+++ compiler/src/main/java/top/ss007/compiler/Processor.java
@@ -6,7 +6,12 @@ import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -16,6 +21,7 @@ import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
@@ -33,7 +39,8 @@ public class Processor extends AbstractProcessor {
     private Messager messager;
     private Elements elementUtils;
 
-
+    //每个存在注解的类整理出来，key:package_classname value:被注解的类型元素
+    private Map<String, List<Element>> annotationClassMap = new HashMap<>();
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
@@ -45,28 +52,36 @@ public class Processor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        if (annotations == null || annotations.isEmpty()) {
+            return false;
+        }
 
         if (!roundEnv.processingOver()) {
+            annotationClassMap.clear();
+            buildAnnotatedElement(roundEnv, BindView.class);
+            buildAnnotatedElement(roundEnv, OnClick.class);
 
-            // find all the classes that uses the supported annotations
-            Set<TypeElement> typeElements = ProcessingUtils.getTypeElementsToProcess(
-                    roundEnv.getRootElements(),  annotations);
-
-            // for each such class create a wrapper class for binding
-            for (TypeElement typeElement : typeElements) {
-                String packageName = elementUtils.getPackageOf(typeElement).getQualifiedName().toString();
-                String typeName = typeElement.getSimpleName().toString();
+            for (Map.Entry<String, List<Element>> entry : annotationClassMap.entrySet()) {
+                String packageName = entry.getKey().split("_")[0];
+                String typeName = entry.getKey().split("_")[1];
                 ClassName className = ClassName.get(packageName, typeName);
-
                 ClassName generatedClassName = ClassName
                         .get(packageName, NameStore.getGeneratedClassName(typeName));
 
-                // define the wrapper class
+                /*
+                创建要生成的类，如下所示
+                @Keep
+                public class MainActivity$Binding {}*/
                 TypeSpec.Builder classBuilder = TypeSpec.classBuilder(generatedClassName)
                         .addModifiers(Modifier.PUBLIC)
                         .addAnnotation(Keep.class);
 
-                // add constructor
+                /*添加构造函数
+                *   public MainActivity$Binding(MainActivity activity) {
+                    bindViews(activity);
+                    bindOnClicks(activity);
+                  }
+                */
                 classBuilder.addMethod(MethodSpec.constructorBuilder()
                         .addModifiers(Modifier.PUBLIC)
                         .addParameter(className, NameStore.Variable.ANDROID_ACTIVITY)
@@ -78,14 +93,19 @@ public class Processor extends AbstractProcessor {
                                 NameStore.Variable.ANDROID_ACTIVITY)
                         .build());
 
-                // add method that maps the views with id
+                /*创建方法bindViews(MainActivity activity)
+                * private void bindViews(MainActivity activity) {}
+                */
                 MethodSpec.Builder bindViewsMethodBuilder = MethodSpec
                         .methodBuilder(NameStore.Method.BIND_VIEWS)
                         .addModifiers(Modifier.PRIVATE)
                         .returns(void.class)
                         .addParameter(className, NameStore.Variable.ANDROID_ACTIVITY);
 
-                for (VariableElement variableElement : ElementFilter.fieldsIn(typeElement.getEnclosedElements())) {
+                /*增加方法体
+                * activity.tvHello = (TextView)activity.findViewById(2131165326);
+                * */
+                for (VariableElement variableElement : ElementFilter.fieldsIn(entry.getValue())) {
                     BindView bindView = variableElement.getAnnotation(BindView.class);
                     if (bindView != null) {
                         bindViewsMethodBuilder.addStatement("$N.$N = ($T)$N.findViewById($L)",
@@ -96,9 +116,18 @@ public class Processor extends AbstractProcessor {
                                 bindView.value());
                     }
                 }
+                //将构建出来的方法添加到类里面
                 classBuilder.addMethod(bindViewsMethodBuilder.build());
 
-                // add method that attaches the onClickListeners
+                /*以下构建如下代码
+                *   private void bindOnClicks(final MainActivity activity) {
+                    activity.findViewById(2131165218).setOnClickListener(new View.OnClickListener() {
+                      public void onClick(View view) {
+                        activity.onHelloBtnClick(view);
+                      }
+                    });
+                  }
+                 */
                 ClassName androidOnClickListenerClassName = ClassName.get(
                         NameStore.Package.ANDROID_VIEW,
                         NameStore.Class.ANDROID_VIEW,
@@ -114,10 +143,10 @@ public class Processor extends AbstractProcessor {
                         .returns(void.class)
                         .addParameter(className, NameStore.Variable.ANDROID_ACTIVITY, Modifier.FINAL);
 
-                for (ExecutableElement executableElement : ElementFilter.methodsIn(typeElement.getEnclosedElements())) {
+                for (ExecutableElement executableElement : ElementFilter.methodsIn(entry.getValue())) {
                     OnClick onClick = executableElement.getAnnotation(OnClick.class);
                     if (onClick != null) {
-                        TypeSpec OnClickListenerClass = TypeSpec.anonymousClassBuilder("")
+                        TypeSpec onClickListenerClass = TypeSpec.anonymousClassBuilder("")
                                 .addSuperinterface(androidOnClickListenerClassName)
                                 .addMethod(MethodSpec.methodBuilder(NameStore.Method.ANDROID_VIEW_ON_CLICK)
                                         .addModifiers(Modifier.PUBLIC)
@@ -132,25 +161,24 @@ public class Processor extends AbstractProcessor {
                         bindOnClicksMethodBuilder.addStatement("$N.findViewById($L).setOnClickListener($L)",
                                 NameStore.Variable.ANDROID_ACTIVITY,
                                 onClick.value(),
-                                OnClickListenerClass);
+                                onClickListenerClass);
                     }
                 }
                 classBuilder.addMethod(bindOnClicksMethodBuilder.build());
 
-                // write the defines class to a java file
+                //将类写入文件中
                 try {
                     JavaFile.builder(packageName,
                             classBuilder.build())
                             .build()
                             .writeTo(filer);
                 } catch (IOException e) {
-                    messager.printMessage(Diagnostic.Kind.ERROR, e.toString(), typeElement);
+                    messager.printMessage(Diagnostic.Kind.ERROR, e.toString());
                 }
             }
         }
         return true;
     }
-
 
 
     @Override
@@ -164,5 +192,23 @@ public class Processor extends AbstractProcessor {
     @Override
     public SourceVersion getSupportedSourceVersion() {
         return SourceVersion.latestSupported();
+    }
+
+    private void buildAnnotatedElement(RoundEnvironment roundEnv, Class<? extends Annotation> clazz) {
+        for (Element element : roundEnv.getElementsAnnotatedWith(clazz)) {
+            String className = getFullClassName(element);
+            List<Element> cacheElements = annotationClassMap.get(className);
+            if (cacheElements == null) {
+                cacheElements = new ArrayList<>();
+                annotationClassMap.put(className, cacheElements);
+            }
+            cacheElements.add(element);
+        }
+    }
+
+    private String getFullClassName(Element element) {
+        TypeElement typeElement = (TypeElement) element.getEnclosingElement();
+        String packageName = elementUtils.getPackageOf(typeElement).getQualifiedName().toString();
+        return packageName + "_" + typeElement.getSimpleName().toString();
     }
 }
